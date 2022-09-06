8f14c04bd874d2df94a0cc038791fbd0e9859b27
DanielAbner17 <107201642+DanielAbner17@users.noreply.github.com>
2022-09-06 16:26:37 -0500

    develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       options: {
         defaults: {
           formats: [`auto`, `webp`],
-          placeholder: `blurred`,
+          placeholder: `none`,
           quality: 95,
           backgroundColor: `transparent`,
         },
